cd0d05c0ba0bcc07c1962b880286b915d759f877
Ravi Tom <rvi.tom@gmail.com>
2020-01-15 14:09:44 +0530

    Updates

---
+++ precache-manifest.088fe5ebc919259f98db1008b126b8bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79957390e90b5396b357137c0202890",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "00ccd18372b703c43745",
+    "url": "/robots/static/css/main.cbdbc172.chunk.css"
+  },
+  {
+    "revision": "2cb855c4e0e118a54326",
+    "url": "/robots/static/js/2.3c31f981.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robots/static/js/2.3c31f981.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00ccd18372b703c43745",
+    "url": "/robots/static/js/main.a6e56b88.chunk.js"
+  },
+  {
+    "revision": "993dc03a480e831e6677",
+    "url": "/robots/static/js/runtime-main.d068132e.js"
+  },
+  {
+    "revision": "bbdb1e35779da88e505cd6b4cc1e9970",
+    "url": "/robots/static/media/cover.bbdb1e35.png"
+  }
+]);
